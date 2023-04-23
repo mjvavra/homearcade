15e42cdf1461e88b7ed35307bb6b76d9ebba9908
mjvavra <mjvavra@hotmail.com>
2023-04-22 20:30:14 -0500

    quick update

--- src/screens/main-menu.tsx
+++ src/screens/main-menu.tsx
@@ -4,8 +4,14 @@ export function Page() {
   return (
     <div>
       <h1>Main Menu</h1>
-      <img alt="house" src={house} />
-      <h1>PRESS ENTER</h1>
+      <div className="main-menu-selection">
+        <button className="main-menu-button" type="button">
+          PICK A SYSTEM
+        </button>
+        <button className="main-menu-button" type="button">
+          OPTIONS
+        </button>
+      </div>
     </div>
   );
 }
